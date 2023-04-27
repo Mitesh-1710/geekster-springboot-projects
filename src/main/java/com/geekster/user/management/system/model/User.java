8a3c863b19320a813dc40fc2f1045933802ba1c8
Mitesh-1710 <mitesh.mayavanshi1710@gmail.com>
2023-04-27 23:56:22 +0530

    Updated model class

--- src/main/java/com/geekster/user/management/system/model/User.java
+++ src/main/java/com/geekster/user/management/system/model/User.java
@@ -9,7 +9,6 @@ import com.geekster.user.management.system.constants.UserManagementSystemConstan
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
